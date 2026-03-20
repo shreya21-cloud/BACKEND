a2abd1a9ca80996c9cff1cf6ab1edf2817bfc679
Shreya Sharma <sharmashreyaa21@gmail.com>
2026-03-20 20:42:48 +0530

    add

---
+++ ASSIGNMENTS/Assignment_arrays/Index.js
@@ -0,0 +1,9 @@
+function indexOf(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(indexOf(['a','b','c'], 'b'));
---
+++ ASSIGNMENTS/Assignment_arrays/MergeArrays.js
@@ -0,0 +1,6 @@
+const a = [1, 2];
+const b = [3, 4];
+
+const result = a.concat(b);
+
+console.log(result); 
